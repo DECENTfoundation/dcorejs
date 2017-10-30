ff950757a7b56afb198519ec55878b2e7c13ce95
Unknown <dusan.drabik@touch4it.com>
2017-10-30 21:15:58 +0100

    [+] initalize lib example

---
+++ examples/Init/index.ts
@@ -0,0 +1,10 @@
+import {Decent} from 'decent-js';
+
+// Initialization
+const config = {
+    decent_network_wspaths: ['wss://your.decent.daemon:8090'],
+    chain_id: 'your-decent-chain-id'
+};
+
+const decent = Decent.instance();
+decent.initialize(config);
