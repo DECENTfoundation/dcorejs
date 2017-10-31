8804e3a3ba8b6c80213ecb168f0ea6543a6da83b
Unknown <dusan.drabik@touch4it.com>
2017-10-31 11:02:12 +0100

    [*] searchContent example fix

---
+++ examples/SearchContent/index.ts
@@ -0,0 +1,29 @@
+import {Content, Decent, SearchParams, SearchParamsOrder} from 'decent-js';
+
+// Initialization
+const config = {
+    decent_network_wspaths: ['wss://your.decent.daemon:8090'],
+    chain_id: 'your-decent-chain-id'
+};
+
+const decent = Decent.instance();
+
+// Content searching
+const term = 'some phrase';
+const order = SearchParamsOrder.createdDesc;
+const user = '1.2.345';
+const region_code = 'en';
+const itemId = '0.0.0';
+const category = '1';
+const count = 4;
+
+const searchParams: SearchParams = new SearchParams(
+    term, order, user, region_code, itemId, category, count
+);
+decent.core.content.searchContent(searchParams)
+    .then((contents: Content[]) => {
+        console.log(contents);
+    })
+    .catch(err => {
+        console.log(err);
+    });
