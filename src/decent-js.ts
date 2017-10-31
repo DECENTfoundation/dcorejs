010f5c877d9fa7882eba79734c76333d15a07775
Duskis <dusan.drabik@touch4it.com>
2017-10-31 08:49:58 +0100

    [+] added exports

--- src/decent-js.ts
+++ src/decent-js.ts
@@ -1,4 +1,4 @@
 export {Core} from './core';
-export {ContentApi, SearchParams} from './content';
+export {ContentApi, SearchParams, Content, Price, Synopsis} from './content';
 export {Account} from './account';
 export {Decent, DecentConfig} from './decent';
