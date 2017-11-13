160a198ea0484706b59bede2e8427479684118b7
Duskis <dusan.drabik@touch4it.com>
2017-11-13 16:20:05 +0100

    [-] remove dist folder

--- examples/Init/index.ts
+++
@@ -1,9 +0,0 @@
-import { Decent } from 'decent-js';
-
-// Initialization
-const config = {
-    decent_network_wspaths: ['wss://your.decent.daemon:8090'],
-    chain_id: 'your-decent-chain-id'
-};
-
-Decent.initialize(config);
