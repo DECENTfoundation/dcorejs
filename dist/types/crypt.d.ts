066f2ee6d5921a79687b3b8e704a7cf57168daed
Duskis <dusan.drabik@touch4it.com>
2017-11-21 14:41:21 +0100

    [-] remove dist folder

--- dist/types/crypt.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference types="node" />
-import { KeyPrivate, KeyPublic } from './utils';
-export declare class CryptoUtils {
-    static encryptWithChecksum(message: string, privateKey: KeyPrivate, publicKey: KeyPublic, nonce?: string): Buffer;
-    static decryptWithChecksum(message: string, privateKey: KeyPrivate, publicKey: KeyPublic, nonce?: string): Buffer;
-    static ripemdHash(fromBuffer: Buffer): string;
-}
