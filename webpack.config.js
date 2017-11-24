bc1652fd25f8b977e23488d53f48029bfc0b9207
Duskis <dusan.drabik@touch4it.com>
2017-11-24 22:07:27 +0100

    WIP webpack experiments vol1

---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+const path = require('path');
+
+module.exports = {
+  entry: './compiled/decent-js.js',
+  output: {
+    filename: 'decent-js.umd.js',
+    path: path.resolve(__dirname, 'dist')
+  }
+};
