c57e85fd0150593b2fb5c7042b91f42588f1996e
Duskis <dusan.drabik@touch4it.com>
2017-11-28 11:11:08 +0100

    [-] removed examples

--- src/examples/SearchContent/searchContent.ts
+++
@@ -1,5 +0,0 @@
-console.log('wuhuhuuuuu');
-// decent.Decent.initialize({
-//     chain_id: '17401602b201b3c45a3ad98afc6fb458f91f519bd30d1058adf6f2bed66376bc',
-//     decent_network_wspaths: ['wss://stage.decentgo.com:8090']
-// });
