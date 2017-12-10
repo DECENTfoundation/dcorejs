25f49b74ab1b7ff8cd728e83586c491be81363bf
Duskis <dusan.drabik@touch4it.com>
2017-12-10 16:22:17 +0100

    [*] bundling settings

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-    entry: './compiled/decent-js.js',
+    entry: './lib/decent-js.js',
     output: {
         filename: 'decent-js.umd.js',
         path: path.resolve(__dirname, 'dist'),
