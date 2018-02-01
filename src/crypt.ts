6d721f142fc2de0a5b345035e160d1f7447e5b1a
Duskis <dusan.drabik@touch4it.com>
2018-02-01 16:42:10 +0100

    [+] encrypt/decrypt with password added, tests

--- src/crypt.ts
+++ src/crypt.ts
@@ -1,8 +1,39 @@
 import { KeyPrivate, KeyPublic } from './utils';
 import { dcorejs_lib } from './helpers';
 import * as md5 from 'crypto-js/md5';
+import * as aes from 'crypto-js/aes';
+import * as cryptoJs from 'crypto-js';
 
 const RIPEMD160 = require('ripemd160');
+
+export class CryptoJSAesJson {
+
+    constructor() {
+    }
+
+    stringify(cipherParams) {
+        const j = {ct: cipherParams.ciphertext.toString(cryptoJs.enc.Base64)};
+        if (cipherParams.iv) {
+            j['iv'] = cipherParams.iv.toString();
+        }
+        if (cipherParams.salt) {
+            j['s'] = cipherParams.salt.toString();
+        }
+        return JSON.stringify(j);
+    }
+
+    parse(jsonStr) {
+        const j = JSON.parse(jsonStr);
+        const cipherParams = cryptoJs.lib.CipherParams.create({ciphertext: cryptoJs.enc.Base64.parse(j.ct)});
+        if (j.iv) {
+            cipherParams.iv = cryptoJs.enc.Hex.parse(j.iv);
+        }
+        if (j.s) {
+            cipherParams.salt = cryptoJs.enc.Hex.parse(j.s);
+        }
+        return cipherParams;
+    }
+}
 
 export class CryptoUtils {
     /**
@@ -35,4 +66,30 @@ export class CryptoUtils {
     public static md5(message: string): string {
         return md5(message).toString();
     }
+
+    public static encrypt(message: string, password: string): string {
+        return aes.encrypt(message, password, {format: CryptoJSAesJson.prototype}).toString();
+    }
+
+    public static decrypt(message: string, password: string): string | null {
+        return aes.decrypt(message, password, {format: CryptoJSAesJson.prototype}).toString(cryptoJs.enc.Utf8) || null;
+    }
+
+    // decrypt(message: string, withPassword: string) {
+    //     return Observable.create(observer => {
+    //         this.messageSender = cryptoJS.AES
+    //             .decrypt(message, withPassword, {format: CryptoJSAesJson.prototype})
+    //             .toString(cryptoJS.enc.Utf8);
+    //         if (!this.messageSender) {
+    //             observer.error(this.messageSender);
+    //         }
+    //         observer.next(this.messageSender);
+    //     });
+    // }
+    //
+    // encrypt(message, password) {
+    //     return cryptoJS.AES
+    //         .encrypt(message, password, {format: CryptoJSAesJson.prototype})
+    //         .toString();
+    // }
 }
--- src/test/client/unit/crypt.test.js
+++ src/test/client/unit/crypt.test.js
@@ -2,6 +2,9 @@ const message = 'test message for encryption';
 const encryptedMessage = '1bcd2ffea59c19cbff34b21e4c3d78947dd0fc8821e211b543395fcb52a35e15';
 const secretWif = '5JDFQN3T8CFT1ynhgd5s574mTV9UPf9WamkHojBL4NgbhSBDmBj';
 const pkeyString = 'DCT5dJjvk9k3yTsnJsAph6V8zEPxsAvJ7FCCzqYWiQQyVTiHvReLz';
+const secretEncrypted = '{"ct":"D2ZtQ9VmbWO0lyK/Ps191onhRBWpkM8FPHIkNw/nT1k=","iv":"5803bd0b0d39e0e216502720c22896ea","s":"3f4d194553206afd"}';
+const email = 'dd.duskis+st14@gmail.com';
+
 let secret = null;
 let pkey = null;
 
@@ -21,5 +24,15 @@ describe('(client/unit) Crypt helper test', () => {
     it('create md5 hash', () => {
         const hash = dcorejs.CryptoUtils.md5('dd.duskis+st14@gmail.com');
         expect(`u${hash}`).to.equal('u5d42a7b0b5713396aac58019eed01d53');
+    }).timeout(5000);
+
+    it('encrypt with password', () => {
+        const encrypted = dcorejs.CryptoUtils.encrypt(email, 'Password1');
+        expect(encrypted).to.be.a('string');
+    }).timeout(5000);
+
+    it('decrypt with password', () => {
+        const encrypted = dcorejs.CryptoUtils.decrypt(secretEncrypted, 'Password1');
+        expect(encrypted).to.equal(email);
     }).timeout(5000);
 });
