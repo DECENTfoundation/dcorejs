b340421f8ce01a66eb21aa8c24131b56cfd96208
dusanDrabik <dusan.drabik@touch4it.com>
2018-05-29 19:01:16 +0200

    [+] listSeedersByPrice method created

--- src/modules/seeding.ts
+++ src/modules/seeding.ts
@@ -1,8 +1,20 @@
 import {ApiModule} from './ApiModule';
 import {DatabaseApi} from '../api/database';
+import {Seeder} from '../model/content';
+import {DatabaseOperations} from '../api/model/database';
+import {SeedingError} from '../model/seeding';
 
 export class SeedingModule extends ApiModule {
     constructor(dbApi: DatabaseApi) {
         super(dbApi);
+    }
+
+    public listSeedersByPrice(limit: number = 100): Promise<Seeder[]> {
+        return new Promise<Seeder[]>((resolve, reject) => {
+            const operation = new DatabaseOperations.ListSeeders(limit);
+            this.dbApi.execute(operation)
+                .then(res => resolve(res))
+                .catch(err => reject(this.handleError(SeedingError.database_operation_failed, err)));
+        });
     }
 }
