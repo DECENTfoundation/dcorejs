eb20729df61f237c5a45c84387929147f0d03404
dusanDrabik <dusan.drabik@touch4it.com>
2018-05-29 18:06:10 +0200

    [+] listActiveSubscriptionByConsumer method added

---
+++ src/model/subscription.ts
@@ -0,0 +1,6 @@
+export interface SubscriptionObject {
+    from: string;
+    to: string;
+    expiration: number;
+    automatic_renewal: boolean;
+}
--- src/modules/subscription.ts
+++ src/modules/subscription.ts
@@ -1,8 +1,21 @@
 import {ApiModule} from './ApiModule';
 import {DatabaseApi} from '../api/database';
+import {DatabaseOperations} from '../api/model/database';
+import {SubscriptionObject} from '../model/subscription';
 
 export class SubscriptionModule extends ApiModule {
     constructor(dbApi: DatabaseApi) {
         super(dbApi);
+    }
+
+    public listActiveSubscriptionByConsumer(consumerId: string, count: number = 100): Promise<SubscriptionObject[]> {
+        return new Promise<SubscriptionObject[]>((resolve, reject) => {
+            const operation = new DatabaseOperations.ListActiveSubscriptionsByConsumer(consumerId, count);
+            this.dbApi.execute(operation)
+                .then(res => {
+                    console.log(res);
+                })
+                .catch(err => reject('databse_execurion_failed'));
+        });
     }
 }
