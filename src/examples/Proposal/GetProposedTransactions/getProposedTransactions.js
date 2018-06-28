30a8ab9e89d8038f15021bb663007628644251e8
kfabianova <katarina.fabianova@touch4it.com>
2018-06-28 13:43:07 +0200

    Add DJS-53 example methods

--- src/examples/Proposal/GetProposedTransactions/getProposedTransactions.js
+++ src/examples/Proposal/GetProposedTransactions/getProposedTransactions.js
@@ -4,7 +4,7 @@ const el = id => document.getElementById(id);
 
 el('searchButton').onclick = () => {
     const keyword = el('keyword').value;
-    listAssets(keyword);
+    getProposedTransactions(keyword);
 };
 const output = el('output');
 
@@ -20,23 +20,20 @@ dcore_js.initialize({
     dcoreNetworkWSPaths: dcoreNetworkAddresses
 }, dcorejs_lib);
 
-function listAssets(symbol) {
+function getProposedTransactions(accountId) {
     output.innerHTML = 'Loading ...';
-    let limit = (symbol !== '') ? 1 : 100;
-    dcore_js.asset().listAssets(symbol, limit)
+    dcore_js.proposal().getProposedTransactions(accountId)
         .then(res => {
             output.innerHTML = '';
-            for (var i = 0; i < res.length; i++) {
+            for (let i = 0; i < res.length; i++) {
                 const result = res[i];
                 output.innerHTML += '<h3>Id: ' + result.id + '</h3>';
-                output.innerHTML += '<h3>Symbol: ' + result.symbol + '</h3>';
-                output.innerHTML += '<h3>Description: ' + result.description + '</h3>';
                 output.innerHTML += JSON.stringify(result, null, 2);
             }
         })
         .catch(err => {
             console.error(err);
-            output.innerHTML = '<p style="color: red;">Error loading assets</p>';
+            output.innerHTML = '<p style="color: red;">Error loading asset</p>';
         });
 }
 
